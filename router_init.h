6ff036ee96ebabba9ee141ca99dd22d5a736712a
jasminej90 <yasmin.aljedawi@yahoo.com>
2013-11-07 22:46:21 -0700

    Update router_init.h

--- router_init.h
+++ router_init.h
@@ -3,18 +3,18 @@
 
 	/* Node or Router structure */
 	typedef struct{
-		char id;			/* ID of the src router */
+		unsigned char id;	/* ID of the src router */
 		char nbr_id;		/* ID of the destination router */
 		int link_cost;		/* Link cost */
 		int tcp_send_port;	/* TCP send port number */
 		int tcp_rec_port;	/* TCP receive port number */
-		struct router *next;
-	}router;
+		struct router_info *next;
+	}router_info;
 	
 	/* Link State Packet structure */
 	typedef struct{
 		char router_id;		/* ID of the src node that created LSP */
-		router *nbrs;		/* List of neighbors and link cost */
+		router_info *nbrs;	/* List of neighbors and link cost */
 		int seq_num;		/* sequence number of LSP */
 		int length;			/* length of LSP */
 		int ttl;			/* Time-to-Live for LSP */
