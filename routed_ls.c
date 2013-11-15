959dfbe6d770ebe8140c84f24f61424ec1fbce89
adnan937 <alsannaa@colorado.edu>
2013-11-15 16:44:10 -0700

    Send now works. All that is left is to do dijkstra and update the table

--- routed_ls.c
+++ routed_ls.c
@@ -5,7 +5,7 @@
 
 #include "router_init.h"
 
-#define LSPSIZE 360 
+#define LSPSIZE 362
 
 
 
@@ -28,19 +28,19 @@ int main(int argc, char *argv[])
 	LSP templsp; 
 	LSP checklsp;
 	lsp.router = r; 
-	lsp.seq_num = 0; 
+	lsp.seq_num = 1; 
 	
 	r = initSock(r);
 	print_router(r);
 	
 	int i;
 	int highSock;
-	char* buffer = malloc(sizeof(lsp));
+	char* buffer = malloc(sizeof(LSPSIZE));
 	bzero(buffer,sizeof(buffer));
 	
-	memcpy(buffer,(char*) &lsp, sizeof(lsp));
-	memcpy((char*) &checklsp, buffer, sizeof(checklsp));
-	printf("router id: %c\nsequence number: %d\n sizeof: %d\n",checklsp.router.ID,checklsp.seq_num, sizeof(lsp));
+	memcpy(buffer,(char*) &lsp, LSPSIZE);
+	memcpy((char*) &checklsp, buffer, LSPSIZE);
+	printf("router id: %c\nsequence number: %d\n sizeof: %d\n",checklsp.router.ID,checklsp.seq_num, (int) sizeof(lsp));
 	
 	
 	fd_set readSet;
@@ -55,7 +55,8 @@ int main(int argc, char *argv[])
 	for(i = 0; i< r.nbrs_count; i++) 
 	{
 		bzero(buffer, sizeof(lsp));
-		if(TCPconnect(r.nbrs[i]) < 0)
+		memcpy(buffer, (char *) &lsp, sizeof(lsp));
+		if(TCPconnect(r.nbrs[i], buffer) < 0)
 			r.nbrs[i].connectedR = 0;
 		else 
 			r.nbrs[i].connectedR = 1;
@@ -100,7 +101,7 @@ int main(int argc, char *argv[])
 			
 			if(FD_ISSET(r.nbrs[i].localSock, &tempReadSet))
 			{	
-				printf("....\n");
+				
 				if( r.nbrs[i].connectedS == 0) 
 				{	
 					int newSock;
@@ -112,13 +113,17 @@ int main(int argc, char *argv[])
 						printf("sock: %d\n",r.nbrs[i].localSock);
 						printf("connectedS: %d\n",r.nbrs[i].connectedS);
 						
-						//FD_SET(r.nbrs[i].localSock,&readSet);
+						FD_SET(r.nbrs[i].localSock,&readSet);
+						
+						if(highSock < newSock)
+							highSock = newSock; 
 					}
 				}
+				
 				else
 				{	
-					printf("in else\n");
-					bzero(buffer,sizeof(360));
+					
+					bzero(buffer,LSPSIZE);
 					int size = sizeof(&r.nbrs[i].localAddr);
 					
 					if(recvfrom(r.nbrs[i].localSock, buffer, LSPSIZE, 0, (struct sockaddr *) &r.nbrs[i].localAddr, &size) <0)
@@ -128,27 +133,28 @@ int main(int argc, char *argv[])
 					else
 					{	
 						printf("file received from %c \n",r.nbrs[i].ID);
-						memcpy((char *)&templsp, buffer, sizeof(templsp));
-						printf("received lsp info...\n");
+						memcpy((char *)&templsp, buffer, LSPSIZE);
 						printf("router id: %c\nsequence number: %d\n",templsp.router.ID,templsp.seq_num);
 					}
 				}
+				
 				
 			}
 		}
 		
 		
-		//printf("116\n");
 	
 		sleep(5);
-		lsp.seq_num = 1 + lsp.seq_num;
+		
 			
 		for(i = 0; i< r.nbrs_count; i++) 
 		{	
 			
 			if(r.nbrs[i].connectedS == 1 &&r.nbrs[i].connectedR == 0)
 			{
-				if(TCPconnect(r.nbrs[i]) < 0)
+				bzero(buffer,sizeof(LSPSIZE));
+				memcpy(buffer,(char*)&lsp, LSPSIZE);
+				if(TCPconnect(r.nbrs[i],buffer) < 0)
 				{
 					printf("failed to connect to %c \n", r.nbrs[i].ID); 
 					r.nbrs[i].connectedR = 0;
@@ -159,18 +165,20 @@ int main(int argc, char *argv[])
 			}
 		}
 		
+		lsp.seq_num++;
 		for(i = 0; i<r.nbrs_count; i++)
 		{
 			if(r.nbrs[i].connectedR == 1) 
 			{
-				buffer = malloc(sizeof(LSPSIZE));
-				bzero(buffer, sizeof(LSPSIZE));
-				memcpy(buffer, (char*) &lsp, sizeof(LSPSIZE)); 
+				buffer = malloc(LSPSIZE);
+				bzero(buffer, LSPSIZE);
+				memcpy(buffer, (char*) &lsp, LSPSIZE);
+				memcpy((char *)&templsp, buffer, LSPSIZE); 
 		
 				if(sendto(r.nbrs[i].remoteSock, buffer, LSPSIZE, 0, (struct sockaddr*)&r.nbrs[i].remoteAddr, sizeof(r.nbrs[i].remoteAddr)) < 0)
 					printf("send failed sending\n");
 		
-				printf("sent LSP to %c\n",r.nbrs[i].ID);
+				printf("sent LSP number %d to %c\n",templsp.seq_num,r.nbrs[i].ID);
 			}
 			
 			
